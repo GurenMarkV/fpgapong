f96f718936995139254533c7e528ae40df700c94
Daksh <GurenMarkV@users.noreply.github.com>
2019-01-13 22:26:58 -0500

    Add files via upload
    code from fpga4fun but not working for hdmi, i dont have vga

---
+++ hdmi_pong.v
@@ -0,0 +1,189 @@
+// (c) fpga4fun.com & KNJN LLC 2013
+
+////////////////////////////////////////////////////////////////////////
+module hdmi_pong(
+	input pixclk,clk,quadA,quadB,  // 25MHz
+	output [2:0] TMDSp, TMDSn,
+	output TMDSp_clock, TMDSn_clock
+
+);
+
+////////////////////////////////////////////////////////////////////////
+reg [9:0] CounterX, CounterY;
+reg hSync, vSync, DrawArea;
+always @(posedge pixclk) DrawArea <= (CounterX<640) && (CounterY<480);
+
+always @(posedge pixclk) CounterX <= (CounterX==799) ? 0 : CounterX+1;
+always @(posedge pixclk) if(CounterX==799) CounterY <= (CounterY==524) ? 0 : CounterY+1;
+
+always @(posedge pixclk) hSync <= (CounterX>=656) && (CounterX<752);
+always @(posedge pixclk) vSync <= (CounterY>=490) && (CounterY<492);
+
+////////////////
+wire [7:0] W = {8{CounterX[7:0]==CounterY[7:0]}};
+wire [7:0] A = {8{CounterX[7:5]==3'h2 && CounterY[7:5]==3'h2}};
+reg [7:0] red, green, blue;
+always @(posedge pixclk) red <= ({CounterX[5:0] & {6{CounterY[4:3]==~CounterX[4:3]}}, 2'b00} | W) & ~A;
+always @(posedge pixclk) green <= (CounterX[7:0] & {8{CounterY[6]}} | W) & ~A;
+always @(posedge pixclk) blue <= CounterY[7:0] | W | A;
+
+////////////////////////////////////////////////////////////////////////
+wire [9:0] TMDS_red, TMDS_green, TMDS_blue;
+TMDS_encoder encode_R(.clk(pixclk), .VD(red  ), .CD(2'b00)        , .VDE(DrawArea), .TMDS(TMDS_red));
+TMDS_encoder encode_G(.clk(pixclk), .VD(green), .CD(2'b00)        , .VDE(DrawArea), .TMDS(TMDS_green));
+TMDS_encoder encode_B(.clk(pixclk), .VD(blue ), .CD({vSync,hSync}), .VDE(DrawArea), .TMDS(TMDS_blue));
+
+////////////////////////////////////////////////////////////////////////
+wire clk_TMDS, DCM_TMDS_CLKFX;  // 25MHz x 10 = 250MHz
+DCM_SP #(.CLKFX_MULTIPLY(10)) DCM_TMDS_inst(.CLKIN(pixclk), .CLKFX(DCM_TMDS_CLKFX), .RST(1'b0));
+BUFG BUFG_TMDSp(.I(DCM_TMDS_CLKFX), .O(clk_TMDS));
+
+////////////////////////////////////////////////////////////////////////
+reg [3:0] TMDS_mod10=0;  // modulus 10 counter
+reg [9:0] TMDS_shift_red=0, TMDS_shift_green=0, TMDS_shift_blue=0;
+reg TMDS_shift_load=0;
+always @(posedge clk_TMDS) TMDS_shift_load <= (TMDS_mod10==4'd9);
+
+always @(posedge clk_TMDS)
+begin
+	TMDS_shift_red   <= TMDS_shift_load ? TMDS_red   : TMDS_shift_red  [9:1];
+	TMDS_shift_green <= TMDS_shift_load ? TMDS_green : TMDS_shift_green[9:1];
+	TMDS_shift_blue  <= TMDS_shift_load ? TMDS_blue  : TMDS_shift_blue [9:1];	
+	TMDS_mod10 <= (TMDS_mod10==4'd9) ? 4'd0 : TMDS_mod10+4'd1;
+end
+
+OBUFDS OBUFDS_red  (.I(TMDS_shift_red  [0]), .O(TMDSp[2]), .OB(TMDSn[2]));
+OBUFDS OBUFDS_green(.I(TMDS_shift_green[0]), .O(TMDSp[1]), .OB(TMDSn[1]));
+OBUFDS OBUFDS_blue (.I(TMDS_shift_blue [0]), .O(TMDSp[0]), .OB(TMDSn[0]));
+OBUFDS OBUFDS_clock(.I(pixclk), .O(TMDSp_clock), .OB(TMDSn_clock));
+//endmodule
+/////////////////////////////////////////////////////////////////
+reg [8:0] PaddlePosition;
+reg [2:0] quadAr, quadBr;
+always @(posedge clk) quadAr <= {quadAr[1:0], quadA};
+always @(posedge clk) quadBr <= {quadBr[1:0], quadB};
+
+always @(posedge clk)
+if(quadAr[2] ^ quadAr[1] ^ quadBr[2] ^ quadBr[1])
+begin
+	if(quadAr[2] ^ quadBr[1])
+	begin
+		if(~&PaddlePosition)        // make sure the value doesn't overflow
+			PaddlePosition <= PaddlePosition + 1;
+	end
+	else
+	begin
+		if(|PaddlePosition)        // make sure the value doesn't underflow
+			PaddlePosition <= PaddlePosition - 1;
+	end
+end
+
+/////////////////////////////////////////////////////////////////
+reg [9:0] ballX;
+reg [8:0] ballY;
+reg ball_inX, ball_inY;
+
+always @(posedge clk)
+if(ball_inX==0) ball_inX <= (CounterX==ballX) & ball_inY; else ball_inX <= !(CounterX==ballX+16);
+
+always @(posedge clk)
+if(ball_inY==0) ball_inY <= (CounterY==ballY); else ball_inY <= !(CounterY==ballY+16);
+
+wire ball = ball_inX & ball_inY;
+/////////////////////////////////////////////////////////////////
+wire border = (CounterX[9:3]==0) || (CounterX[9:3]==79) || (CounterY[8:3]==0) || (CounterY[8:3]==59);
+wire paddle = (CounterX>=PaddlePosition+8) && (CounterX<=PaddlePosition+120) && (CounterY[8:4]==27);
+wire BouncingObject = border | paddle; // active if the border or paddle is redrawing itself
+
+reg ResetCollision;
+always @(posedge clk) ResetCollision <= (CounterY==500) & (CounterX==0);  // active only once for every video frame
+
+reg CollisionX1, CollisionX2, CollisionY1, CollisionY2;
+always @(posedge clk) if(ResetCollision) CollisionX1<=0; else if(BouncingObject & (CounterX==ballX   ) & (CounterY==ballY+ 8)) CollisionX1<=1;
+always @(posedge clk) if(ResetCollision) CollisionX2<=0; else if(BouncingObject & (CounterX==ballX+16) & (CounterY==ballY+ 8)) CollisionX2<=1;
+always @(posedge clk) if(ResetCollision) CollisionY1<=0; else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY   )) CollisionY1<=1;
+always @(posedge clk) if(ResetCollision) CollisionY2<=0; else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY+16)) CollisionY2<=1;
+
+/////////////////////////////////////////////////////////////////
+wire UpdateBallPosition = ResetCollision;  // update the ball position at the same time that we reset the collision detectors
+
+reg ball_dirX, ball_dirY;
+always @(posedge clk)
+if(UpdateBallPosition)
+begin
+	if(~(CollisionX1 & CollisionX2))        // if collision on both X-sides, don't move in the X direction
+	begin
+		ballX <= ballX + (ball_dirX ? -1 : 1);
+		if(CollisionX2) ball_dirX <= 1; else if(CollisionX1) ball_dirX <= 0;
+	end
+
+	if(~(CollisionY1 & CollisionY2))        // if collision on both Y-sides, don't move in the Y direction
+	begin
+		ballY <= ballY + (ball_dirY ? -1 : 1);
+		if(CollisionY2) ball_dirY <= 1; else if(CollisionY1) ball_dirY <= 0;
+	end
+end 
+
+/////////////////////////////////////////////////////////////////
+wire R = BouncingObject | ball | (CounterX[3] ^ CounterY[3]);
+wire G = BouncingObject | ball;
+wire B = BouncingObject | ball;
+
+reg vga_R, vga_G, vga_B;
+always @(posedge clk)
+begin
+	vga_R <= R & inDisplayArea;
+	vga_G <= G & inDisplayArea;
+	vga_B <= B & inDisplayArea;
+end
+
+endmodule
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+module TMDS_encoder(
+	input clk,
+	input [7:0] VD,  // video data (red, green or blue)
+	input [1:0] CD,  // control data
+	input VDE,  // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
+	output reg [9:0] TMDS = 0
+);
+
+wire [3:0] Nb1s = VD[0] + VD[1] + VD[2] + VD[3] + VD[4] + VD[5] + VD[6] + VD[7];
+wire XNOR = (Nb1s>4'd4) || (Nb1s==4'd4 && VD[0]==1'b0);
+wire [8:0] q_m = {~XNOR, q_m[6:0] ^ VD[7:1] ^ {7{XNOR}}, VD[0]};
+
+reg [3:0] balance_acc = 0;
+wire [3:0] balance = q_m[0] + q_m[1] + q_m[2] + q_m[3] + q_m[4] + q_m[5] + q_m[6] + q_m[7] - 4'd4;
+wire balance_sign_eq = (balance[3] == balance_acc[3]);
+wire invert_q_m = (balance==0 || balance_acc==0) ? ~q_m[8] : balance_sign_eq;
+wire [3:0] balance_acc_inc = balance - ({q_m[8] ^ ~balance_sign_eq} & ~(balance==0 || balance_acc==0));
+wire [3:0] balance_acc_new = invert_q_m ? balance_acc-balance_acc_inc : balance_acc+balance_acc_inc;
+wire [9:0] TMDS_data = {invert_q_m, q_m[8], q_m[7:0] ^ {8{invert_q_m}}};
+wire [9:0] TMDS_code = CD[1] ? (CD[0] ? 10'b1010101011 : 10'b0101010100) : (CD[0] ? 10'b0010101011 : 10'b1101010100);
+
+always @(posedge clk) TMDS <= VDE ? TMDS_data : TMDS_code;
+always @(posedge clk) balance_acc <= VDE ? balance_acc_new : 4'h0;
+endmodule
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
